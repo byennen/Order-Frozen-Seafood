a0ac0883e5468baf7d83b462836e6a61a627ffa1
Lance Ennen <byennen@gmail.com>
2010-11-27 17:27:15 -0600

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrderfrozenseafoodCom::Application.config.secret_token = '37613ac81f5f99bb9239ebfcf98bd1a69653dbc09b3d2d725080c9ffa0da47a0caad56d029f8f9f41b43adfce0a213b7ba6600e02c1f3fd3dc3bd741f950e00a'
